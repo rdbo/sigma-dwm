43a1848ac034ecfc399e16d68afad6a11b3d75a8
user <user@localhost>
2022-10-13 14:17:06 -0300

    added rule for prompts

--- config.h
+++ config.h
@@ -29,8 +29,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
+/*	{ "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
 	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "wmprompt", NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
