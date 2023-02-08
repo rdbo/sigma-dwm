0d1e6dae6339f2e350891b46daf739a0a28ee7f5
rdbo <rdbodev@gmail.com>
2023-02-08 11:50:04 -0300

    added swallow patch

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font:size=10" };
@@ -28,10 +29,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-/*	{ "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
-/*	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 }, */
-	{ "wmprompt", NULL,       NULL,       0,            1,           -1 },
+	/* class      instance    title           tags mask     isfloating   isterminal   noswallow   monitor */
+	{ "wmprompt", NULL,       NULL,           0,            1,           1,           1,          -1 },
+	{ "st",       NULL,       NULL,           0,            0,           1,           0,          -1 },
+	{ NULL,       NULL,       "Event Tester", 0,            0,           0,           1,          -1 },
 };
 
 /* layout(s) */
